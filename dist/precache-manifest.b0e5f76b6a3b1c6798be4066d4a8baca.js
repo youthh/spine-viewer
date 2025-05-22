6103f7a1dd2eb08126ef7b77fe8d5bd73cf7e066
youthh <valavskiylesha630@gmail.comgit config user.namegit config user.email valavskiylesha630@gmail.com>
2025-05-22 15:16:11 +0300

    removed avif

---
+++ dist/precache-manifest.b0e5f76b6a3b1c6798be4066d4a8baca.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11ee5042c37877e4bf0a",
+    "url": "/pixi-spine-viewer/css/app.02f55a00.css"
+  },
+  {
+    "revision": "7e8f3770aa9d4db49211",
+    "url": "/pixi-spine-viewer/css/chunk-vendors.6c05c4ea.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/pixi-spine-viewer/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/pixi-spine-viewer/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "aa919fe098b83ff9a8387c9c50fce1a7",
+    "url": "/pixi-spine-viewer/index.html"
+  },
+  {
+    "revision": "11ee5042c37877e4bf0a",
+    "url": "/pixi-spine-viewer/js/app.d779ed89.js"
+  },
+  {
+    "revision": "7e8f3770aa9d4db49211",
+    "url": "/pixi-spine-viewer/js/chunk-vendors.4196acb2.js"
+  },
+  {
+    "revision": "88dae7d4a7abcc5d1d1603b9178d7eb4",
+    "url": "/pixi-spine-viewer/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/pixi-spine-viewer/robots.txt"
+  }
+]);
